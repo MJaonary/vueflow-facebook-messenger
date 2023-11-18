b2eaf3a949ee17a07b9985a73acbbcf71512464d
Rajaonarivony Lantonirina Onitiana Mamisoa <mamisoa@wau.co>
2023-11-19 01:35:14 +0300

    Importing Roboto font.

--- src/App.vue
+++ src/App.vue
@@ -248,7 +248,7 @@ body,
 }
 
 #app {
-  font-family: Roboto;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
